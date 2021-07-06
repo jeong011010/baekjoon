331eae1f92e7bd776d9cc6bb2e93c51267465167
jeong011010 <jeong011010@naver.com>
2021-07-06 19:20:16 +0900

    Add files via upload

---
+++ C++ 배우기 (51~100)/2522.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n,j;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (j = i+1; j < n; j++)
+			cout << ' ';
+		for (j = 0; j < i + 1; j++)
+			cout << '*';
+		cout << '\n';
+	}
+	for (int i = n-1; i > 0; i--) {
+		for (j = n; j > i; j--)
+			cout << ' ';
+		for (j = i; j > 0; j--)
+			cout << '*';
+		cout << '\n';
+	}
+	return 0;
+}

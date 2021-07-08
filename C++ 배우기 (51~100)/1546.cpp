255a270cb3cf86bf9549e4e80d9aa8bb9f4aa761
jeong011010 <jeong011010@naver.com>
2021-07-08 23:05:29 +0900

    Add files via upload

---
+++ C++ 배우기 (51~100)/1546.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n, max=0;
+	double sum = 0;
+	cin >> n;
+	int* tmp = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> tmp[i];
+		if (tmp[i] > max) max = tmp[i];
+	}
+	for (int i = 0; i < n; i++) sum += tmp[i] / (double)max * 100;
+	cout << (double)sum / (double)n;
+	return 0;
+}

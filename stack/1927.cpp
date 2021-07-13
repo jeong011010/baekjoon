fffa78cd31838bbfd7bf399b362bad5f896e7a28
jeong011010 <jeong011010@naver.com>
2021-07-13 15:22:52 +0900

    Add files via upload

--- stack/1927.cpp
+++ stack/1927.cpp
@@ -1,27 +1,30 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
+#include <iostream>  
+#include <functional>
+#pragma warning(disable: 4996) 
 using namespace std;
 
 // global
 priority_queue<int, vector<int>, greater<int> > pq;
-int n;
+int N;
 
 int main() {
-    cin >> n;
-    for (int n_idx = 0; n_idx < n; n_idx++) {
+    scanf("%d", &N);
+    for (int n_idx = 0; n_idx < N; n_idx++) {
         int _input_int;
-        cin >> _input_int;
+        scanf("%d", &_input_int);
 
         // 0이면 하나 빼서 출력
         // 아니면 삽입 
         switch (_input_int) {
         case 0:
             if (pq.size()) {
-                cout << pq.top() << '\n';
+                printf("%d\n", pq.top());
                 pq.pop();
             }
             else {
-                cout << "0\n";
+                printf("%d\n", 0);
             }
             break;
         default:

8150fceed01b7d6daab6c64b28f66cf9bc6c75c0
jeong011010 <jeong011010@naver.com>
2021-08-09 16:21:40 +0900

    Add files via upload

--- solved.ac/1107/1107.cpp
+++ solved.ac/1107/1107.cpp
@@ -1,1 +1,49 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+using namespace std;
 
+int arr[10] = { 0 };
+int inf = 1000007;
+int n, m, i, click_num = 0, click = 0;
+
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+bool possible(int k) {
+	if (k == 0)
+		return arr[0] ? false : true;
+	while (k) {
+		if (arr[k % 10] == 1)
+			return false;
+		k /= 10;
+	}
+	return true;
+}
+
+int check(int n) {
+	int hun = abs(n - 100);
+	int button_min = inf;
+	int tmp;
+	for (int i = 0; i <= inf; i++) {
+		if (possible(i)) {
+			tmp = to_string(i).length();
+			tmp += abs(i - n);
+			button_min = min(button_min, tmp);
+		}
+	}
+	return min(hun, button_min);
+}
+
+int main() {
+	int n, m, tmp;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> tmp;
+		arr[tmp] = 1;
+	}
+	cout << check(n) << '\n';
+	return 0;
+
+}
